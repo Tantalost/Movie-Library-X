cc5929e6a2d594158c67b96d51bb724fc724616f
Lol <133163245+Tantalost@users.noreply.github.com>
2025-10-07 11:34:33 +0800

    fix: cleaning up unncessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Movies
 import MoviesList from "./pages/movies/MoviesList";
 import MovieDetails from "./pages/movies/MovieDetails";
 import MovieReviews from "./pages/movies/MovieReviews";
 
-// People
-import PeopleList from "./pages/people/PeopleList";
-
-// Static pages
 import About from "./pages/About";
 import Home from "./pages/Home";
 
@@ -16,19 +11,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Home route */}
         <Route path="/" element={<Home />} />
 
-        {/* Movies routes */}
         <Route path="/movies" element={<MoviesList defaultSort="rating" />} />
         <Route path="/movies/:id" element={<MovieDetails />}>
           <Route path="reviews" element={<MovieReviews />} />
         </Route>
 
-        {/* People route */}
-        <Route path="/people" element={<PeopleList />} />
-
-        {/* About route */}
         <Route path="/about" element={<About author="Group 3" />} />
       </Routes>
     </Router>
